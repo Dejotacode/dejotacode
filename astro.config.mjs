d4fab219ddc3c72f89a55e79d0c3ef7f777c06b4
Joceilton F. Santos <joceilton1989@gmail.com>
2026-09-15 19:19:37 -0300

    perf: comprime HTML e atualiza sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,13 @@ import sitemap from "@astrojs/sitemap";
 export default defineConfig({
   site: "https://dejotacode.com.br",
   output: "static",
+  compressHTML: true,
   integrations: [
     sitemap({
       filter: (page) =>
         ![
           "https://dejotacode.com.br/busca/",
           "https://dejotacode.com.br/guia/iniciante-em-tecnologia/",
-          "https://dejotacode.com.br/categoria/inteligencia-artificial/",
-          "https://dejotacode.com.br/categoria/tecnologia-pratica/",
         ].includes(page),
     }),
   ],
